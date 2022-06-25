e3a37ff9877476b4f48400d23e17b6ef67251263
Mikko Huttunen <mikkohuttunen.92@gmail.com>
2022-06-25 16:04:54 +0300

    Projects mockup & fine tuning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { MatDividerModule } from '@angular/material/divider';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
 
 import { ProfileComponent } from './profile/profile.component';
 import { ProjectsComponent } from './projects/projects.component';
@@ -38,6 +40,8 @@ import { FooterComponent } from './footer/footer.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatDividerModule,
+    MatCardModule,
     FlexLayoutModule,
   ],
   providers: [],
